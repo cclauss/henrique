6797e02faafcb8ec7bc4463016a16a4986331943
Diogo Osório <diogo.g.osorio@gmail.com>
2014-04-24 21:51:24 +0100

    Added some initial packaging instructions.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Henrique',
+    version='0.1dev',
+    packages=['henrique'],
+    license='Beerware',
+    long_description=open('README').read(),
+)
