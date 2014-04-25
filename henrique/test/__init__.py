a929ddaf1bf692a2cb1bf87b4de50fa29ebbcc02
Diogo Osório <diogo.g.osorio@gmail.com>
2014-04-25 17:09:14 +0100

    Registered the UiFactoryTest test case.

--- henrique/test/__init__.py
+++ henrique/test/__init__.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import utils
 
 class TestSuite(object):
 
     def __init__(self):
         self.suite = unittest.TestSuite()
+        self.suite.addTest(utils.UiFactoryTest())
 
     def run(self):
         runner = unittest.TextTestRunner()
