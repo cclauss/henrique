569b42a41f124505cca879a84bf89b390e126e41
Diogo Osório <diogo.g.osorio@gmail.com>
2014-04-25 20:51:59 +0100

    Implemented the application home creation, database copy methods.

--- henrique/application.py
+++ henrique/application.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import argparse
+import os
+import shutil
+import getopt
 from PyQt4 import QtCore, QtGui
 from utils import UiFactory
 from ui.henrique import Ui_Henrique
+
+DATABASE = 'henrique.db'
 
 class Henrique(object):
 
@@ -14,7 +18,29 @@ class Henrique(object):
         self.setupEnvironment()
 
     def setupEnvironment(self):
-        pass
+        argv = self.argv[1:]
+        optlist, args = getopt.getopt(argv, '', ['home-dir='])
+
+        for opt, val in optlist:
+            if opt == '--home-dir':
+                self.home = val
+
+        self.makeHomeDirectory()
+        self.copyDatabase()
+
+    def copyDatabase(self):
+        dbfile = os.path.join(self.home, DATABASE)
+
+        if not os.path.exists(dbfile):
+            currentpath = os.path.dirname(os.path.realpath(__file__))
+            sourcefile = os.path.realpath(os.path.join(currentpath, '..', 'db', DATABASE))
+            shutil.copy2(sourcefile, dbfile)
+
+    def makeHomeDirectory(self):
+        self.home = self.home if hasattr(self, 'home') else os.path.expanduser('~/.henrique')
+
+        if not os.path.exists(self.home):
+            os.makedirs(self.home, mode = 0755)
 
     def start(self):
         app = QtGui.QApplication(self.argv)
--- henrique/test/application.py
+++ henrique/test/application.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import os
 from henrique.application import Henrique
+from henrique.application import DATABASE
 
 class HenriqueTest(unittest.TestCase):
 
     def test_argument_parsing(self):
-        args = ['run.py', '--HOME=/tmp']
+        args = ['run.py', '--home-dir=/tmp']
         app = Henrique(args)
         self.assertEquals("/tmp", app.home)
 
-        args = ['--HOME=/tmp']
+        app = Henrique([])
+        self.assertNotEquals(app.home, None)
+
+    def test_create_database(self):
+        args = ['run.py', '--home-dir=/tmp']
         app = Henrique(args)
-        self.assertEquals("/tmp", app.home)
+
+        dbfile = os.path.realpath(os.path.join("/tmp", DATABASE))
+        self.assertTrue(os.path.exists(dbfile), "Failed asserting that the database file exists")
