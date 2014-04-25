b7a8c37a88618b674f094c50fe8734beabbd9493
Diogo Osório <diogo.g.osorio@gmail.com>
2014-04-25 17:17:47 +0100

    Fixed some logic issues with the UiFactoryTest.

--- henrique/test/utils.py
+++ henrique/test/utils.py
@@ -7,18 +7,18 @@ import henrique.utils
 class UiFactoryTest(unittest.TestCase):
 
     def setUp(self):
-        self.uifactory = utils.UiFactory(events_module="henrique.test.mock.event",
+        self.uifactory = henrique.utils.UiFactory(events_module="henrique.test.mock.event",
                 ui_module="henrique.test.mock.ui")
 
     def test_modules(self):
         self.assertEqual("henrique.test.mock.event", self.uifactory.events_module)
-        self.assertEqual("henrique.test.mock.event", self.uifactory.ui_module)
+        self.assertEqual("henrique.test.mock.ui", self.uifactory.ui_module)
 
     def test_make_class_generation(self):
         event_manager = self.uifactory.make('MainWindow')
         self.assertIsInstance(event_manager, henrique.test.mock.event.MainWindowEventManager)
-        self.assertIsInstance(event_manager.ui, henrique.test.mock.ui.Ui_MainWindow)
+        self.assertIsInstance(event_manager.ui, henrique.test.mock.ui.mainwindow.Ui_MainWindow)
 
     def test_make_invalid_class(self):
-        self.assertRaises(exceptions.ImportError, self.uifactory.make('UnexistentWIndow'))
+        self.assertRaises(exceptions.ImportError, self.uifactory.make('UnexistentWindow'))
 
