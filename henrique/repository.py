151516319dbbea802bb376e1660c49d37ef24acc
Diogo Osório <diogo.g.osorio@gmail.com>
2014-04-25 22:41:19 +0100

    Implemented the findByDate method on the ReportRepository.

--- henrique/repository.py
+++ henrique/repository.py
@@ -5,9 +5,20 @@ import sqlite3
 class Repository(object):
     def __init__(self, app):
         self.connection = sqlite3.connect(app.dbfile)
+        self.connection.row_factory = self.cursorToDictionary
+
+    def cursorToDictionary(self, cursor, row):
+        d = {}
+        for idx, col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+
+        return d
 
 class ReportRepository(Repository):
     STATUS_SENT = 1
     STATUS_NOT_SENT = 0
 
-    pass
+    def findByDate(self, date):
+        query = "SELECT * FROM report WHERE date=date(?)"
+        cursor = self.connection.execute(query, (date.isoformat(),))
+        return cursor.fetchall()
