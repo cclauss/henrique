6ffea9a146901a14b337d77e128da098ffb20f1d
Diogo Osório <diogo.g.osorio@gmail.com>
2014-04-26 00:46:45 +0100

    Renamed utils to helpers.

---
+++ henrique/helpers.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from repository import ReportRepository
+
+class MainWindowHelper(object):
+
+    def __init__(self, ui, report):
+        self.ui = ui
+        self.report = report
+
+    def refreshUi(self):
+        self.ui.TasksEditor.setText(self.report['content'])
+        self.updateStatus()
+
+    def updateStatus(self):
+        text = 'This report wasn\'t sent'
+        style = 'color: red'
+
+        if self.report['status'] == ReportRepository.STATUS_SENT:
+            text = 'This report was already sent'
+            style = 'color: green'
+
+        self.ui.StatusLabel.setText(text)
+        self.ui.StatusLabel.setStyleSheet(style)
