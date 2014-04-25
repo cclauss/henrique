ee4e4117339ddf9063df8e2f7b90689f9063766e
Diogo Osório <diogo.g.osorio@gmail.com>
2014-04-25 21:41:58 +0100

    Added a dummy ReportRepository class.

--- henrique/repository.py
+++ henrique/repository.py
@@ -3,6 +3,11 @@
 import sqlite3
 
 class Repository(object):
-
     def __init__(self, app):
         self.connection = sqlite3.connect(app.dbfile)
+
+class ReportRepository(Repository):
+    STATUS_SENT = 1
+    STATUS_NOT_SENT = 0
+
+    pass
