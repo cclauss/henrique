09bb8563ea8500faacecfdeb625efbd03c0e1d4c
Diogo Osório <diogo.g.osorio@gmail.com>
2014-04-26 02:52:38 +0100

    Imeplemented a hacky way to persist the writes on Sqlite

--- henrique/models.py
+++ henrique/models.py
@@ -7,6 +7,8 @@ class Model(object):
 
     DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
     DATE_FORMAT = "%Y-%m-%d"
+    WRITES_PENDING = 0
+    MAX_WRITES_PENDING = 50
 
     datetime_fields = []
 
@@ -28,6 +30,13 @@ class Model(object):
     def parseDateTime(self, datetimestr):
         return datetime.strptime(datetimestr, self.DATETIME_FORMAT)
 
+    def flagWrite(self):
+        self.WRITES_PENDING += 1
+
+        if (self.WRITES_PENDING >= self.MAX_WRITES_PENDING):
+            self.connection.commit()
+            self.WRITES_PENDING = 0
+
 
 class ReportModel(Model):
     STATUS_SENT = 1
@@ -38,11 +47,15 @@ class ReportModel(Model):
     def findByDate(self, date):
         query = "SELECT *, strftime('%Y-%m-%d', date) AS report_date FROM report WHERE report_date=(?)"
         cursor = self.connection.execute(query, (date.strftime(self.DATE_FORMAT),))
-        return cursor.fetchall()
+        reports = cursor.fetchall()
+        cursor.close()
+        return reports
 
     def findById(self, id):
         cursor = self.connection.execute("SELECT * FROM report WHERE id=?", (id,))
-        return cursor.fetchone()
+        report = cursor.fetchone()
+        cursor.close()
+        return report
 
     def create(self, date=None, content='', status=0):
         create_date = datetime.now().strftime(self.DATETIME_FORMAT)
@@ -51,10 +64,18 @@ class ReportModel(Model):
 
         query = "INSERT INTO report(date, create_date, update_date, content, status) VALUES (?, ?, ?, ?, ?)"
         cursor.execute(query, (date, create_date, create_date, content, status,))
+        cursor.close()
+
+        self.flagWrite()
 
         return cursor.lastrowid
 
     def update(self, id, content='', status=0):
         update_date = datetime.now().strftime(self.DATETIME_FORMAT)
         cursor = self.connection.cursor()
-        cursor.execute("UPDATE report SET update_date=?, content=?, status=? WHERE id=?", (update_date, content, status, id,))
+        query = "UPDATE report SET update_date=?, content=?, status=? WHERE id=?"
+        params = (update_date, content, status, id,)
+        cursor.execute(query, params)
+        cursor.close()
+
+        self.flagWrite()
